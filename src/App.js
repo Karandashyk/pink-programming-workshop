c48704a9c3dc97a18ace506a8a65d77cb9d99ee8
Daria Sharshakova <sharshakova@MacBook-Air-Daria.local>
2021-11-14 01:38:38 +0100

    Working example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const friends = [
+    {
+      id: 1,
+      name: "Anna",
+      favoriteColor: "violet",
+    },
+    {
+      id: 2,
+      name: "Alice",
+      favoriteColor: "pink",
+    },
+    {
+      id: 3,
+      name: "Kate",
+      favoriteColor: "lightseagreen",
+    },
+    {
+      id: 4,
+      name: "Frida",
+      favoriteColor: "olive",
+    },
+    {
+      id: 5,
+      name: "Ivanna",
+      favoriteColor: "lightskyblue",
+    },
+    {
+      id: 6,
+      name: "Minesa",
+      favoriteColor: "orange",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Pink Programming</h1>
       </header>
+      <main className="main">
+        {friends.map((friend) => (
+          <div
+            className="card"
+            style={{ backgroundColor: friend.favoriteColor }}
+          >
+            <span>#{friend.id}</span>
+            <h4>{friend.name}</h4>
+          </div>
+        ))}
+      </main>
+      <footer className="footer">
+        <span className="text-code">I have {friends.length} new friends!</span>
+      </footer>
     </div>
   );
 }
